817ef6b8d32650a872d34159f2361494f5b3460b
Leonardo Alfaya Fonseca <leonardo.fonseca@invillia.com>
2021-03-17 23:02:46 -0300

    chore: initial config

---
+++ config/express.js
@@ -0,0 +1,12 @@
+
+const express    = require('express');
+const config     = require('config');
+
+module.exports = () => {
+  const app = express();
+
+  // SETANDO VARIÁVEIS DA APLICAÇÃO
+  app.set('port', process.env.PORT || config.get('server.port'));
+
+  return app;
+}
